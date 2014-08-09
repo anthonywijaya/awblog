2e9b4b06dcd827962a1f1ad4ea91de986828f694
Anthony <longeverlasting33@gmail.com>
2014-08-10 04:02:14 +0700

    Updates analytic.js

--- js/analytics.js
+++ js/analytics.js
@@ -4,4 +4,5 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-53664187-1', 'auto');
+  ga('require', 'displayfeatures');
   ga('send', 'pageview');
